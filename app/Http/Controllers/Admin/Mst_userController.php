e530b48eccfe026c2fee21d94d907165ac10e4ea
dvthang <dvthang@brights.vn>
2016-04-25 23:24:55 +0700

    fixxx

--- app/Http/Controllers/Admin/Mst_userController.php
+++ app/Http/Controllers/Admin/Mst_userController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Mst_user;
 use App\Task;
+use DB;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -12,7 +13,7 @@ use App\Http\Controllers\Controller;
 class Mst_userController extends Controller
 {
     function index(){
-        $tasks = Task::where('user_id', '1')->get();
-        var_dump($tasks[0]);exit;
+        $users = DB::table('mst_users')->get();
+        return view('admin/users')->with('users',$users);
     }
 }
